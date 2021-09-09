8d2d95094af2473e5e0b40972aaca02fa0cede78
Tanushree-H <76546168+Tanushree-H@users.noreply.github.com>
2021-09-09 15:00:21 +0530

    Home page - top search bar, bottom navigation, 2 carousels, single image list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import ButtonAppBar from './Components/AppBar.js'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Pages/Home.js'
 import Gallery from './Pages/Gallery.js'
+
 
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       {/* Header (Navigation Bar) */}
       <header className="App-header">
-      <ButtonAppBar/>
+      
       </header>
       
       {/* Router - Pages */}
@@ -18,10 +18,11 @@ function App() {
             <Route path={process.env.PUBLIC_URL + '/'} exact > <Home/> </Route>
             <Route path={process.env.PUBLIC_URL + '/home'} > <Home/> </Route>
             <Route path={process.env.PUBLIC_URL + '/gallery'}  > <Gallery/> </Route>
+            
           </Switch>
 
       <footer className="App-footer">
-        This is a footer
+        
       </footer>
     </div>
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
-import DatePickers from '../Components/DateTimePicker';
+import SimpleBottomNavigation from '../Components/BottomNavigation';
+import PrimarySearchAppBar from '../Components/TopSearchBar';
+import SwipeableTextMobileStepper from '../Components/CarouselMenu';
+import SingleLineImageList from '../Components/SingleImageList';
 
 export default function Home() {
   return (
     <React.Fragment>
-        <Container fixed>
-            <p>
-                Welcome to your web application. Make something beautiful!
-            </p>
-            <p>
-                To get started, edit <code>src/App.js</code>, save, and reload this page.
-            </p>
-            <DatePickers/>
+        <Container maxWidth="sm">
+        <PrimarySearchAppBar/>
+        <SwipeableTextMobileStepper/>
+        <SwipeableTextMobileStepper/>
+        <SingleLineImageList/>
+          <SimpleBottomNavigation/>
+        
         </Container>
     </React.Fragment>
   );
